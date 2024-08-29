e323deaa0bd9ff0dd2b83dddf8a8de0702df5ca2
gabrielapalooza <gabriellagonzag@gmail.com>
2024-08-29 16:04:43 -0500

    Funciones single linked

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -41,8 +41,81 @@ def get_element(lista,pos):
         current["next"]
         
 def delete_element(lista,pos):
-    if is_empty(lista) == True:
+    if is_empty(lista) or pos <= 0 or pos > lista["size"]:
         raise IndexError("la lista no puede estar vacia")
     else:
-        
-        
+        i = 0
+        nodo_actual = lista["first"]
+        while i < pos - 1:
+            nodo_actual = nodo_actual["next"]
+            i += 1
+        removed = nodo_actual["next"]
+        nodo_actual["next"] = removed["next"]
+    lista["size"] -= 1
+    return lista
+
+
+    
+def remove_first(lista):
+    if is_empty(lista): 
+        raise IndexError("la lista no puede estar vacia")
+    primero = lista["first"]
+    siguiente = primero["next"]
+    lista["first"] = siguiente 
+    lista["size"] -= 1
+    return primero
+    
+def remove_last(lista):
+    if is_empty(lista):
+         raise IndexError("la lista no puede estar vacia")
+    else: 
+        nodo = lista["first"]
+        i = 0
+        while i < lista["size"]-2:
+            nodo_actual = nodo_actual["next"]
+            i += 1
+        ultimo_nodo = nodo_actual["next"]
+        nodo_actual["next"] = None
+        lista["size"] -= 1
+        return ultimo_nodo
+
+def insert_element(lista, elemento, pos):
+    if pos < 0 or pos > lista["size"]:
+        raise IndexError("Posición fuera de rango.")
+    
+    nuevo_nodo = {"info": elemento, "next": None} 
+    if pos == 0:
+        nuevo_nodo["next"] = lista["first"]
+        lista["first"] = nuevo_nodo
+    else:
+        nodo_actual = lista["first"]
+        i = 0
+        while i < pos - 1:
+            nodo_actual = nodo_actual["next"]
+            i+=1
+        nuevo_nodo["next"] = nodo_actual["next"]
+        nodo_actual["next"] = nuevo_nodo
+    lista["size"] += 1
+    return lista   
+
+def is_present(lista, element, cmp_function):
+    nodo_actual = lista["first"]
+    i = 0
+    while nodo_actual is not None:
+        if cmp_function(nodo_actual["data"], element) == 0:
+            return i
+        nodo_actual = nodo_actual["next"]
+        i += 1
+    return -1
+
+def change_info(lista, pos, new_info):
+    if pos < 0 or pos >= lista["size"]:
+        raise IndexError("Posición fuera de rango.")
+
+    nodo_actual = lista["first"]
+    i = 0
+    while i < pos:
+        nodo_actual = nodo_actual["next"]
+        i += 1
+    nodo_actual["data"] = new_info
+    return lista
